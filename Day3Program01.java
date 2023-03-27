996c54c0e0406b8849dcefaf9742752a5b1d4b58
knagajoshna090 <129029357+knagajoshna090@users.noreply.github.com>
2023-03-27 01:00:26 -0400

    Add files via upload

---
+++ Day3Program01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Day3Program01
+{
+public static void main(String args[])
+{
+Scanner scob=new Scanner(System.in);
+int num1=scob.nextInt();//19
+int num2=scob.nextInt();//4
+int res1=num1/num2;
+int res2=num1%num2;
+System.out.println(res1);
+System.out.println(res2);
+}//main()
+}//Program01
