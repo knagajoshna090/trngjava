4b082d08b85fde0542f17ad0dedfd5e67af91796
knagajoshna090 <129029357+knagajoshna090@users.noreply.github.com>
2023-03-27 01:00:54 -0400

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Program2
+{
+public static void main(string args[])
+{
+Scanner sc=new Scanner(System.in);
+int num1=sc.nextInt();//2
+int num2=sc.nextInt();//3
+int num3=sc.nextInt();//4
+int res=num1+num2*num3;
+//2+3*4=14
+//System.out.println(num1,"+",num2,"*",num3,"=",res);
+System.out.println(num1+"+"+num2+"*"+num3+"="+res);
+}//main()
+}//Program2
+//96/4/2=12
